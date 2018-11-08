c39205967c70fa6636b399726690eb363b1b365d
Alessandro Genova <ales.genova@gmail.com>
2018-11-07 22:05:23 -0500

    Update dependencies and instructions

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { defineCustomElements as defineMolecule } from '@openchemistry/molecule-vtkjs';
-import { defineCustomElements as defineSplitMe } from 'split-me';
+import { defineCustomElements as defineMolecule } from '@openchemistry/molecule-vtkjs/dist/loader';
+import { defineCustomElements as defineSplitMe } from 'split-me/dist/loader';
 
 defineMolecule(window);
 defineSplitMe(window);
